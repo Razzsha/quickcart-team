ccff44ad93a7c418b7b7d7686f5eb5bc50fa3ae0
toxicboi99 <shaharyan93277@gmail.com>
2026-02-24 22:10:10 +0530

    Updated code3

--- backend/web.js
+++ backend/web.js
@@ -1,5 +1,6 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const puppeteer = require('puppeteer');
 
 let whatsappClient = null;
 let isReady = false;
